216ee761ae16f3118c5be8befbb8d33f3d9ee658
Juan Andres M <juan131marrio@gmail.com>
2024-11-13 17:40:15 -0500

    Prueba de actualizacion de parametros

--- camarones/src/app/linechart/linechart.component.ts
+++ camarones/src/app/linechart/linechart.component.ts
@@ -9,23 +9,23 @@ Chart.register(...registerables);
   standalone: true,
   imports: [RouterOutlet, RouterLink],
   templateUrl: './linechart.component.html',
-  styleUrl: './linechart.component.css'
+  styleUrl: './linechart.component.css',
 })
 export class LinechartComponent implements OnInit {
-
+  // Configuración de gráficos
   public lineConfig1: any = {
     type: 'line',
     data: {
-      labels: ['ENERO','JUNIO', 'DICIEMBRE'],
+      labels: ['SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'],
       datasets: [
         {
           label: 'OXIGENO',
-          data: [0.8, 35, 105],
+          data: [0.8, 35, 105, 50],
           fill: false,
           borderColor: '#A8699F',
           tension: 0.1,
           borderWidth: 3,
-          pointRadius: 5
+          pointRadius: 5,
         },
       ],
     },
@@ -38,7 +38,7 @@ export class LinechartComponent implements OnInit {
           const datasetIndex = elements[0].datasetIndex;
           const dataIndex = elements[0].index;
           const value = this.lineConfig1.data.datasets[datasetIndex].data[dataIndex];
-          this.updateSemaforo('semaforo1', value);
+          this.updateSemaforo('semaforo1', value, { red: [100, 150], yellow: [50, 99], green: [0.5, 49] });
         }
       },
     },
@@ -47,17 +47,17 @@ export class LinechartComponent implements OnInit {
   public lineConfig2: any = {
     type: 'line',
     data: {
-      labels: ['ENERO','JUNIO', 'DICIEMBRE'],
+      labels: ['SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'],
       datasets: [
         {
           label: 'TEMPERATURA',
-          data: [6, 70, 500],
+          data: [25, 24, 30, 35],
           fill: false,
           borderColor: '#FF336B',
           tension: 0.1,
           borderWidth: 3,
-          pointRadius: 5
-        }
+          pointRadius: 5,
+        },
       ],
     },
     options: {
@@ -69,7 +69,7 @@ export class LinechartComponent implements OnInit {
           const datasetIndex = elements[0].datasetIndex;
           const dataIndex = elements[0].index;
           const value = this.lineConfig2.data.datasets[datasetIndex].data[dataIndex];
-          this.updateSemaforo('semaforo2', value);
+          this.updateSemaforo('semaforo2', value, { red: [31, 35], yellow: [16, 24], green: [25, 30] });
         }
       },
     },
@@ -78,17 +78,17 @@ export class LinechartComponent implements OnInit {
   public lineConfig3: any = {
     type: 'line',
     data: {
-      labels: ['ENERO','JUNIO', 'DICIEMBRE'],
+      labels: ['SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'],
       datasets: [
         {
           label: 'SALINIDAD',
-          data: [3.6, 60, 200],
+          data: [25, 16, 35, 40],
           fill: false,
           borderColor: '#3498DB',
           tension: 0.1,
           borderWidth: 3,
-          pointRadius: 5
-        }
+          pointRadius: 5,
+        },
       ],
     },
     options: {
@@ -100,7 +100,7 @@ export class LinechartComponent implements OnInit {
           const datasetIndex = elements[0].datasetIndex;
           const dataIndex = elements[0].index;
           const value = this.lineConfig3.data.datasets[datasetIndex].data[dataIndex];
-          this.updateSemaforo('semaforo3', value);
+          this.updateSemaforo('semaforo3', value, { red: [36, 40], yellow: [16, 24], green: [25, 35] });
         }
       },
     },
@@ -109,17 +109,17 @@ export class LinechartComponent implements OnInit {
   public lineConfig4: any = {
     type: 'line',
     data: {
-      labels: ['ENERO','JUNIO', 'DICIEMBRE'],
+      labels: ['SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'],
       datasets: [
         {
           label: 'PH',
-          data: [0.5, 40, 100],
+          data: [7.5, 8.5, 14, 6.5],
           fill: false,
           borderColor: '#2ECC71',
           tension: 0.1,
           borderWidth: 3,
-          pointRadius: 5
-        }
+          pointRadius: 5,
+        },
       ],
     },
     options: {
@@ -131,7 +131,7 @@ export class LinechartComponent implements OnInit {
           const datasetIndex = elements[0].datasetIndex;
           const dataIndex = elements[0].index;
           const value = this.lineConfig4.data.datasets[datasetIndex].data[dataIndex];
-          this.updateSemaforo('semaforo4', value);
+          this.updateSemaforo('semaforo4', value, { red: [9, 14], yellow: [6.5, 7.4], green: [7.5, 8.5] });
         }
       },
     },
@@ -159,28 +159,34 @@ export class LinechartComponent implements OnInit {
     }
   }
 
-  updateSemaforo(semaforoId: string, value: number): void {
+  updateSemaforo(
+    semaforoId: string,
+    value: number,
+    thresholds: { red: [number, number]; yellow: [number, number]; green: [number, number] }
+  ): void {
     const redLight = document.getElementById(`${semaforoId}-red`) as HTMLElement;
     const yellowLight = document.getElementById(`${semaforoId}-yellow`) as HTMLElement;
     const greenLight = document.getElementById(`${semaforoId}-green`) as HTMLElement;
     const semaforoMessage = document.getElementById(`${semaforoId}-message`) as HTMLElement;
 
     if (redLight && yellowLight && greenLight && semaforoMessage) {
+      // Resetear colores
       redLight.style.backgroundColor = 'grey';
       yellowLight.style.backgroundColor = 'grey';
       greenLight.style.backgroundColor = 'grey';
 
-      semaforoMessage.textContent = "Estado del valor";
+      semaforoMessage.textContent = 'Estado del valor';
 
-      if (value >= 100) {
+      // Evaluar rango de valores
+      if (value >= thresholds.red[0] && value <= thresholds.red[1]) {
         redLight.style.backgroundColor = 'red';
-        semaforoMessage.textContent = "Valor negativo";
-      } else if (value >= 35) {
+        semaforoMessage.textContent = 'Valor negativo';
+      } else if (value >= thresholds.yellow[0] && value <= thresholds.yellow[1]) {
         yellowLight.style.backgroundColor = 'yellow';
-        semaforoMessage.textContent = "Valor regular";
-      } else if (value >= 0.5) {
+        semaforoMessage.textContent = 'Valor regular';
+      } else if (value >= thresholds.green[0] && value <= thresholds.green[1]) {
         greenLight.style.backgroundColor = 'green';
-        semaforoMessage.textContent = "Valor positivo";
+        semaforoMessage.textContent = 'Valor positivo';
       }
     } else {
       console.error('No se pudieron encontrar los elementos del semáforo');
